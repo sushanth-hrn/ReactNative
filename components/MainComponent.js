a87935bd78bdd991114989159bed94de784a0ceb
sushanth <sushanth.nukala14@gmail.com>
2021-05-18 23:47:03 +0530

    drawer content setup

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,9 +1,9 @@
 import React , { Component } from 'react';
-import { View, Platform } from "react-native";
+import { View, Platform, Image, StyleSheet, ScrollView, Text } from "react-native";
 import { createStackNavigator } from "react-navigation-stack";
-import { createDrawerNavigator } from "react-navigation-drawer";
-import { createAppContainer } from "react-navigation";
-import { Icon } from "react-navigation-elements";
+import { createDrawerNavigator, DrawerItems } from "react-navigation-drawer";
+import { createAppContainer, SafeAreaView } from "react-navigation";
+import { Icon } from "react-native-elements";
 import Dishdetail from './DishdetailComponent';
 import Menu from './MenuComponent';
 import Home from './HomeComponent';
@@ -86,6 +86,24 @@ const ContactNavigator = createStackNavigator({
     })
 });
 
+const CustomDrawerContentComponent = (props) => (
+    <ScrollView>
+        <SafeAreaView style={styles.container}
+                forceInset={{ top : 'always' , horizontal : 'never'}}>
+            <View style={styles.drawerHeader}>
+                <View style={{ flex : 1 }}>
+                    <Image source={{uri:'.images/logo.png'}}
+                        style={styles.drawerImage}/>
+                </View>
+                <View style={{ flex : 2 }}>
+                    <Text style={styles.drawerHeaderText}>Restaurant ConFusion</Text>
+                </View>
+            </View>
+            <DrawerItems {...props}/>
+        </SafeAreaView>
+    </ScrollView>
+)
+
 const MainNavigator = createDrawerNavigator({
     Home : {
         screen : HomeNavigator,
@@ -149,6 +167,7 @@ const MainNavigator = createDrawerNavigator({
     }
 }, {
     drawerBackgroundColor : '#D1C4E9',
+    contentComponent : CustomDrawerContentComponent
 })
 
 const MainContainer = createAppContainer(MainNavigator);
@@ -162,4 +181,28 @@ class Main extends Component {
 
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    drawerHeader : {
+        backgroundColor: '#512DA8',
+        height: 140,
+        alignItems: 'center',
+        justifyContent: 'center',
+        flex: 1,
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: 'white',
+        fontSize: 24,
+        fontWeight: 'bold',
+        fontStyle: 'italic',
+    },
+    drawerImage: {
+        margin: 10,
+        width: 80,
+        height: 60
+    }
+})
 export default Main;
